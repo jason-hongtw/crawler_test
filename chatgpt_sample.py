e9bf90826ffda739c2e197c43fe336fea232886e
Your Name <you@example.com>
2025-02-20 17:25:40 +0800

    add chatgpt fuction more

--- chatgpt_sample.py
+++ chatgpt_sample.py
@@ -21,3 +21,9 @@ def chat_with_gpt(user_message, system_prompt):
     )
     # pprint(completion)
     print(completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    chat_with_gpt(
+        system_prompt = "你是一個飲料店店員，協助點餐",
+        user_message ="我要一個珍珠奶茶"
+    )
