64a4b10c383a480d5f24b55a5a215706bc099c9b
Your Name <you@example.com>
2025-02-21 12:10:11 +0800

    fix chatgpt fuction

--- chatgpt_sample.py
+++ chatgpt_sample.py
@@ -4,9 +4,12 @@ client = OpenAI()
 
 
 
-def chat_with_gpt(user_message, system_prompt):
+def chat_with_gpt(
+    user_message,
+    system_prompt,
+    completion_model="gpt-4o-mini"):
     completion = client.chat.completions.create(
-    model="gpt-4o-mini", # replace any model you want
+    model = completion_model,
     messages=[
         {
             "role": "system",
@@ -20,8 +23,9 @@ def chat_with_gpt(user_message, system_prompt):
 
     )
     # pprint(completion)
-    print(completion.choices[0].message.content)
+    return completion.choices[0].message.content
 
+# 當這個檔案被執行時，以下的程式碼才會被執行,__name__是一個變數，當這個檔案被執行時，__name__會被設定為"__main__"
 if __name__ == "__main__":
     chat_with_gpt(
         system_prompt = "你是一個飲料店店員，協助點餐",
